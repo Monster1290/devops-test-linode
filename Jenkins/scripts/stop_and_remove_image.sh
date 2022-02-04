b1335422b1fcdb16424046510a705abd5db5f99e
Alexander Besedin <besedin.a.a@yandex.ru>
2022-02-04 15:50:00 +0500

    fix return code check logic and add verbosity

--- Jenkins/scripts/stop_and_remove_image.sh
+++ Jenkins/scripts/stop_and_remove_image.sh
@@ -2,9 +2,14 @@
 
 image_id=$(docker stop app)
 
-if [[ $! -eq 0 ]]; then
+# shellcheck disable=SC2181
+if [[ $? -eq 0 ]]; then
+  echo "Container stopped"
   docker rm app
+  echo "Container removed"
   docker rmi "${image_id}"
+  echo "Old image removed"
 else
+  echo "No running container on this instance"
   exit 0
 fi
